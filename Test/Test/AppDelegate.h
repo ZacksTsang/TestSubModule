792ecc93af0ae1fce34b3f7d018c1909e4b49616
Zacks Tsang <zshaojia@gmail.com>
2013-07-30 11:38:45 +0800

    add Project

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Zacks Tsang  on 13-7-29.
+//  Copyright (c) 2013年 Zacks Tsang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
